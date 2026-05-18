1a9cc3973e41de74c976b1fa20cbfd503fc5ef5f
Dag Frode Solberg <dag.solberg@sparebank1.no>
2026-05-18 17:23:05 +0200

    Legg til ikonanalyse

--- indeks-docs/src/theme/MDXComponents.tsx
+++ indeks-docs/src/theme/MDXComponents.tsx
@@ -1,8 +1,10 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import { MobilbankOnly } from '@site/src/components/MobilbankOnly';
+import IconAnalyseTable from '@site/src/components/IconAnalyseTable';
 
 // Registrerer egne komponenter slik at MDX-forfattere kan bruke dem uten import.
 export default {
     ...MDXComponents,
     MobilbankOnly,
+    IconAnalyseTable,
 };
