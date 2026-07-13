05e27fbb58600e2e7fe8d4e504175ee19752be84
Dag Frode Solberg <dag.solberg@sparebank1.no>
2026-07-13 10:22:14 +0200

    Trekk ut html bruk av combobox

--- indeks-docs/sidebars.ts
+++ indeks-docs/sidebars.ts
@@ -127,7 +127,12 @@ const sidebars: SidebarsConfig = {
                             link: { type: 'doc', id: 'komponenter/skjema/checkbox-group' },
                             items: ['komponenter/skjema/checkbox-group-designvalg'],
                         },
-                        'komponenter/skjema/combobox',
+                        {
+                            type: 'category',
+                            label: 'Combobox',
+                            link: { type: 'doc', id: 'komponenter/skjema/combobox' },
+                            items: ['komponenter/skjema/combobox-html'],
+                        },
                         'komponenter/skjema/label',
                         {
                             type: 'category',
